b455957c6b94490b6b832ecd6a5c29ca37918822
emily.c.ritter <emily.c.ritter@gmail.com>
2016-03-28 09:01:11 -0500

    Add relationships for models

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,13 @@
 class Room < ActiveRecord::Base
+  has_many :users, through: :members
+
+  validates :name, :user_id, presence: true
+  validates :name, uniqueness: true
+
+  attr_reader :room_paramaterize
+
+  def room_paramaterize
+    "#{id}-#{name.parameterize}"
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  has_many :rooms, through: :members
+  has_many :posts, dependent: :destroy
+
+  validates :name, :email, :password, presence: true
+  validates :name, :email, uniqueness: true
+
 end
